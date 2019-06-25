a8af5d993f46653195a611a1648b001ca9a440d8
Jeremy Ng Cheng Hin <jngcheng@sfu.ca>
2019-06-24 17:21:31 -0700

    Adding new tests for grid constraints

--- src/index.test.js
+++ src/index.test.js
@@ -5,3 +5,26 @@ test('Random Value', () =>{
     expect(value).toBeGreaterThanOrEqual(0);
     expect(value).toBeLessThan(10);
 });
+
+describe('Testing grid conditions', ()=>{
+    const grid = [[2,3,5,null,null,null,null,null,null],
+                  [null,null,null,null,null,null,null,null,null],
+                  [null,null,null,null,null,null,null,null,null],
+                  [null,null,null,null,null,null,null,null,null],
+                  [null,null,null,null,null,null,null,null,null],
+                  [null,null,null,null,null,null,null,null,null],
+                  [null,null,null,null,null,null,null,null,null],
+                  [null,null,null,null,null,null,null,null,null],
+                  [null,null,null,null,null,null,null,null,null]];
+    const colNumber = 1;
+    const rowNumber = 0;
+    const valToAdd = 3;
+
+    it('Error inserting same value into same column',()=>{
+        expect(fn.checkCol(grid,colNumber,valToAdd)).toBe(false);
+    });
+
+    it('Error inserting same value into same row',()=>{
+        expect(fn.checkRow(grid,rowNumber,valToAdd)).toBe(false);
+    })
+});
