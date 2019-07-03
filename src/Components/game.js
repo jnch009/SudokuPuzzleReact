9ac76583a7d290ddd9e30865d6f3210022e54608
Jeremy Ng Cheng Hin <jngcheng@sfu.ca>
2019-07-02 22:02:21 -0700

    App is now responsive for mobile

--- src/Components/game.js
+++ src/Components/game.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Board from './board';
-import {Square} from './square';
 
 class Game extends React.Component {
   render() {
       return (
         <div className="game">
           <div className="game-title">
-            SUDOKU!
+            <p className="title">SUDOKU!</p>
           </div>
           <div className="game-board">
-            <div className="sudoku"></div>
+            <div className="sudoku">
+              <Board/>
+            </div>
           </div>
           <div className="creator-info">
             <p id="creatorName">Developed by: Jeremy Ng Cheng Hin</p>
