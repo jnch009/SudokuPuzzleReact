b36b09111ce32043866d110da7fe6f03f8be6d65
Jeremy Ng Cheng Hin <jngcheng@sfu.ca>
2019-07-02 17:54:23 -0700

    css styling

--- src/Components/game.js
+++ src/Components/game.js
@@ -5,13 +5,11 @@ class Game extends React.Component {
   render() {
       return (
         <div className="game">
+          <div className="game-title">SUDOKU!</div>
           <div className="game-board">
             <Board />
           </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol></ol>
-          </div>
+          <div className="creator-info">Created by: Jeremy Ng Cheng Hin</div>
         </div>
       );
     }
