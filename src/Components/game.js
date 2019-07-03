eed1c312bd4151bf8af515258e5490eb6ca59a21
Jeremy Ng Cheng Hin <jngcheng@sfu.ca>
2019-07-02 21:06:39 -0700

    experimenting with css responsiveness

--- src/Components/game.js
+++ src/Components/game.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Board from './board';
+import {Square} from './square';
 
 class Game extends React.Component {
   render() {
       return (
         <div className="game">
-          <div className="game-title">SUDOKU!</div>
-          <div className="game-board">
-            <Board />
+          <div className="game-title">
+            SUDOKU!
           </div>
-          <div className="creator-info">Created by: Jeremy Ng Cheng Hin</div>
+          <div className="game-board">
+            <div className="sudoku"></div>
+          </div>
+          <div className="creator-info">
+            <p id="creatorName">Developed by: Jeremy Ng Cheng Hin</p>
+          </div>
         </div>
       );
     }
