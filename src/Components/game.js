e7ba37ffc999f8bbf6371b0b11f87764a1c07623
Jeremy Ng Cheng Hin <jngcheng@sfu.ca>
2019-07-03 18:13:14 -0700

    more css styling, and new game functionality

--- src/Components/game.js
+++ src/Components/game.js
@@ -20,12 +20,16 @@ class Game extends React.Component {
     this.handleCreditsClick = this.handleCreditsClick.bind(this);
     this.handleDifficultyClick = this.handleDifficultyClick.bind(this);
     this.handleRulesClick = this.handleRulesClick.bind(this);
-    //this.handleNewGameClick = this.handleNewGameClick.bind(this);
+    this.handleNewGameClick = this.handleNewGameClick.bind(this);
     this.changeDifficulty = this.changeDifficulty.bind(this);
   }
 
   changeDifficulty(diff){
     this.setState(() => ({difficulty: diff}));
+  }
+
+  newGameAccepted(){
+    this.setState(() => ({newGame: true}));
   }
 
   handleDifficultyClick(){
@@ -40,6 +44,10 @@ class Game extends React.Component {
     this.setState(() => ({openRules: !this.state.openRules}));
   }
 
+  handleNewGameClick(){
+    this.setState(() => ({openNewGame: !this.state.openNewGame}));
+  }
+
   render() {
       return (
         <div className="game">
@@ -47,14 +55,14 @@ class Game extends React.Component {
             <p className="title">SUDOKU!</p>
           </div>
           <div className="game-board">
-            <Board difficulty={this.state.difficulty}/>
+            <Board difficulty={this.state.difficulty} newGame={this.state.newGame}/>
           </div>
           <Container className="dr-example-container">
               <Row>
                 <Col><Button onClick={this.handleCreditsClick} className="navBar">Credits</Button></Col>
                 <Col><Button onClick={this.handleDifficultyClick} className="navBar">Difficulty</Button></Col>
                 <Col><Button onClick={this.handleRulesClick} className="navBar">How To Play</Button></Col>
-                <Col><Button className="navBar">New Game</Button></Col>
+                <Col><Button onClick={this.handleNewGameClick} className="navBar">New Game</Button></Col>
               </Row>
           </Container>
 
@@ -78,18 +86,32 @@ class Game extends React.Component {
             </ModalBody>
           </Modal>
 
-          <Modal open={this.state.openRules} toggle={this.handleRulesClick}>
+          <Modal open={this.state.openRules}>
             <ModalHeader>Welcome to Sudoku!</ModalHeader>
             <ModalBody>
+              <div className="rulesText">
               1. Only one number from 1-9 is allowed on each row<br></br>
               2. Only one number from 1-9 is allowed on each column<br></br>
               3. Only one number from 1-9 is allowed in each grid<br></br>
               The goal of the game is to find the missing numbers in the grid such that all three of these conditions are satisfied and if they are then you have successfully completed the puzzle.<br></br>
               If not, then you must backtrack and find out which numbers are inserted incorrectly.<br></br>
               You will know if the number is inserted incorrectly when the box is highlighted red.<br></br>
+              </div>
+              <Button onClick={this.handleRulesClick}>Got it!</Button>
             </ModalBody>
           </Modal>
 
+          <Modal open={this.state.openNewGame}>
+            <ModalBody>
+              <div className="newGameText">
+                Are you sure?<br></br>
+              </div>
+              <div className="flexButtons">
+                <Button>Yes</Button>
+                <Button onClick={this.handleNewGameClick}>No</Button>
+              </div>
+            </ModalBody>
+          </Modal>
         </div>
       );
     }
