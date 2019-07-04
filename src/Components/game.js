2c5d095d6b92310e7d086400d18771352d2e2be0
Jeremy Ng Cheng Hin <jngcheng@sfu.ca>
2019-07-03 17:04:14 -0700

    Added functionality for credits button

--- src/Components/game.js
+++ src/Components/game.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import Board from './board';
-import { Container, Row, Col, Button } from "shards-react";
+import { Container, Row, Col, Button,
+         Modal, ModalBody, ModalHeader } from "shards-react";
 
 class Game extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      openCredits: false,
+    };
+
+    this.handleCreditsClick = this.handleCreditsClick.bind(this);
+  }
+
+  handleCreditsClick(){
+    this.setState(() => ({openCredits: !this.state.openCredits}));
+  }
+
   render() {
       return (
         <div className="game">
@@ -14,12 +30,16 @@ class Game extends React.Component {
           </div>
           <Container className="dr-example-container">
               <Row>
-                <Col><Button className="navBar">Credits</Button></Col>
+                <Col><Button onClick={this.handleCreditsClick} className="navBar">Credits</Button></Col>
                 <Col><Button className="navBar">Difficulty</Button></Col>
                 <Col><Button className="navBar">How To Play</Button></Col>
                 <Col><Button className="navBar">New Game</Button></Col>
               </Row>
           </Container>
+          <Modal open={this.state.openCredits} toggle={this.handleCreditsClick}>
+            <ModalHeader>Credits</ModalHeader>
+            <ModalBody>Developed by: Jeremy Ng Cheng Hin</ModalBody>
+          </Modal>
         </div>
       );
     }
