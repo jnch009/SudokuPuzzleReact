aa80d2a2e0e0cef7e55fb3bd980d2b7939792f52
jnch009 <jngcheng@sfu.ca>
2020-05-22 17:16:27 -0700

    fixing difficulty modal

--- src/Components/game.js
+++ src/Components/game.js
@@ -132,7 +132,10 @@ class Game extends React.Component {
           <ModalBody>Developed by: Jeremy Ng Cheng Hin</ModalBody>
         </Modal>
 
-        <Modal open={this.state.openDifficulty}>
+        <Modal
+          open={this.state.openDifficulty}
+          toggle={this.handleDifficultyClick}
+        >
           <ModalHeader>Change Difficulty</ModalHeader>
           <ModalBody>
             <FormRadio
@@ -175,16 +178,29 @@ class Game extends React.Component {
           <ModalHeader>Welcome to Sudoku!</ModalHeader>
           <ModalBody>
             <div className='rulesText'>
-              1. Only one number from 1-9 is allowed on each row<br></br>
-              2. Only one number from 1-9 is allowed on each column<br></br>
-              3. Only one number from 1-9 is allowed in each grid<br></br>
-              The goal of the game is to find the missing numbers in the grid
-              such that all three of these conditions are satisfied and if they
-              are then you have successfully completed the puzzle.<br></br>
-              If not, then you must backtrack and find out which numbers are
-              inserted incorrectly.<br></br>
-              You will know if the number is inserted incorrectly when the box
-              is highlighted red.<br></br>
+              <p>
+                1. Only one number from 1-9 is allowed on each row<br></br>
+              </p>
+              <p>
+                2. Only one number from 1-9 is allowed on each column<br></br>
+              </p>
+              <p>
+                3. Only one number from 1-9 is allowed in each grid<br></br>
+              </p>
+              <p>
+                The goal of the game is to find the missing numbers in the grid
+                such that all three of these conditions are satisfied and if
+                they are then you have successfully completed the puzzle.
+                <br></br>
+              </p>
+              <p>
+                If not, then you must backtrack and find out which numbers are
+                inserted incorrectly.<br></br>
+              </p>
+              <p>
+                You will know if the number is inserted incorrectly when the box
+                is highlighted red.<br></br>
+              </p>
             </div>
             <Button onClick={this.handleRulesClick}>Got it!</Button>
           </ModalBody>
