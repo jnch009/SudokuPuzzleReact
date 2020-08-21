2e8313b4bf60ea157264efde26e5bbdbda5764d2
Jeremy Ng <jngchenghin@gmail.com>
2020-08-20 22:16:28 -0700

    adding a protected route

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,36 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
 
 const connect = require('./connect');
 
 connect.run().catch(console.dir);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Authentication middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and 
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://jnch009.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'https://jnch009/sudoku',
+  issuer: 'https://jnch009.auth0.com/',
+  algorithms: ['RS256']
+});
+
+app.get('/sudoku/', checkJwt, (req, res) => {
+  res.send('Protected Route');
 });
 
 app.listen(port, () => {
