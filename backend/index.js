b4f2d04bf49a286eedc811d4a3204319a408fb62
Jeremy Ng <jngchenghin@gmail.com>
2020-08-21 10:29:12 -0700

    Protecting API with JWT (#11)
    * added MongoDB connection
    
    * experimenting with querying and adding data
    
    * having some funzies with MongoDB
    
    * adding a protected route
    
    * env variables for jwks_uri, audience and issuer
    
    * adding scope validation and keeping insert-data.js as a reference
    
    * cleaning up comments

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,32 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 3000;
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const connect = require('./connect');
+
+connect.run().catch(console.dir);
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: process.env.JWKS_URI
+  }),
+
+  audience: process.env.AUDIENCE,
+  issuer: process.env.ISSUER,
+  algorithms: ['RS256']
+});
+
+const checkScopes = jwtAuthz([ 'read:saves' ]);
+
+app.get('/sudoku/', checkJwt, checkScopes, (req, res) => {
+  res.send('Protected Route');
 });
 
 app.listen(port, () => {
