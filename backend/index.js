baf91a6a3d46c568be941a484ad3a22d8684c823
Jeremy Ng <jngchenghin@gmail.com>
2020-08-24 16:48:24 -0700

    TDD DELETE implementation

--- backend/index.js
+++ backend/index.js
@@ -169,7 +169,35 @@ app.delete(
   checkJwt,
   checkScopes,
   async (req, res) => {
-    res.json("game deleted");
+    try {
+      const db = client.db(
+        process.env['NODE_ENV'] === 'test' ? 'test' : dbName
+      );
+      const col = db.collection('saves');
+      const getSaves = await col.findOne({ _id: req.params.userId });
+
+      if (getSaves === null) {
+        res.status(400).json(errorMessages.USER_NON_EXISTENT);
+      } else {
+        if (getSaves.saves[req.params.saveGame] === undefined) {
+          res.status(400).json(errorMessages.SAVE_GAME_NOT_FOUND);
+        } else {
+          getSaves.saves.splice(req.params.saveGame, 1);
+
+          await col.updateOne(
+            { _id: req.params.userId },
+            {
+              $set: {
+                saves: getSaves.saves,
+              },
+            }
+          );
+          res.json(await col.findOne({ _id: req.params.userId }));
+        }
+      }
+    } catch (err) {
+      res.status(400).json(err.stack);
+    }
   }
 );
 
