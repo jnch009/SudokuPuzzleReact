f4b95fd45f5ac9ec7828e3a84970ed704563943f
Jeremy Ng <jngchenghin@gmail.com>
2020-08-28 17:28:47 -0700

    transitioning to top nav bar to make things a little easier

--- frontend/src/Components/game.js
+++ frontend/src/Components/game.js
@@ -3,6 +3,7 @@ import Board from './board';
 import Login from '../Components/Login/Login';
 import Logout from '../Components/Logout/Logout';
 import Profile from '../Components/Profile/Profile';
+import SideNav from '../Components/SideNav/SideNav';
 
 import {
   Container,
@@ -155,159 +156,160 @@ class Game extends React.Component {
     }
 
     return (
-      <div className='game'>
-        <div className='game-title'>
-          <p className='title'>SUDOKU!</p>
-        </div>
-        <div className='game-board'>
-          <Board
-            difficulty={this.state.difficulty}
-            newGame={this.state.newGame}
-            populateGameGrid={this.populateGameGrid}
-            solvedButton={this.state.solvedButton}
-            solvedGrid={this.state.grid}
-          />
-        </div>
-        <Container className='dr-example-container'>
-          <Row>
-            <Col>
-              <Link to='/credits'>
-                <Button onClick={this.handleCreditsClick} className='navBar'>
+      <>
+      {/* <SideNav /> */}
+          <Container>
+            <Row>
+              <Col>
+                <Link to='/credits'>
+                  <Button onClick={this.handleCreditsClick} className='navBar'>
                   Credits
-                </Button>
-              </Link>
-            </Col>
-            <Col>
-              <Link to='/difficulty'>
-                <Button onClick={this.handleDifficultyClick} className='navBar'>
+                  </Button>
+                </Link>
+              </Col>
+              <Col>
+                <Link to='/difficulty'>
+                  <Button onClick={this.handleDifficultyClick} className='navBar'>
                   Difficulty
-                </Button>
-              </Link>
-            </Col>
-            <Col>
-              <Button onClick={this.handleSudokuSolver} className='navBar'>
+                  </Button>
+                </Link>
+              </Col>
+              <Col>
+                <Button onClick={this.handleSudokuSolver} className='navBar'>
                 Solve
-              </Button>
-            </Col>
-            <Col>
-              <Link to='/rules'>
-                <Button onClick={this.handleRulesClick} className='navBar'>
+                </Button>
+              </Col>
+              <Col>
+                <Link to='/rules'>
+                  <Button onClick={this.handleRulesClick} className='navBar'>
                   How To Play
-                </Button>
-              </Link>
-            </Col>
-            <Col>
-              <Link to='/profile'>
-                <Button className='navBar'>Profile</Button>
-              </Link>
-            </Col>
-            <Col>
-              <Link to='/newGame'>
-                <Button onClick={this.handleNewGameClick} className='navBar'>
+                  </Button>
+                </Link>
+              </Col>
+              <Col>
+                <Link to='/profile'>
+                  <Button className='navBar'>Profile</Button>
+                </Link>
+              </Col>
+              <Col>
+                <Link to='/newGame'>
+                  <Button onClick={this.handleNewGameClick} className='navBar'>
                   New Game
-                </Button>
-              </Link>
-            </Col>
-            <Col>{isAuthenticated ? <Logout /> : <Login />}</Col>
-          </Row>
+                  </Button>
+                </Link>
+              </Col>
+              <Col>{isAuthenticated ? <Logout /> : <Login />}</Col>
+            </Row>
 
-          <Switch>
-            <PrivateRoute path='/profile' component={Profile} />
-          </Switch>
-        </Container>
+            <Switch>
+              <PrivateRoute path='/profile' component={Profile} />
+            </Switch>
+          </Container>
 
-        <Modal open={this.state.openCredits} toggle={this.handleCreditsClick}>
-          <ModalHeader>Credits</ModalHeader>
-          <ModalBody>Developed by: Jeremy Ng Cheng Hin</ModalBody>
-        </Modal>
+          <Modal open={this.state.openCredits} toggle={this.handleCreditsClick}>
+            <ModalHeader>Credits</ModalHeader>
+            <ModalBody>Developed by: Jeremy Ng Cheng Hin</ModalBody>
+          </Modal>
 
-        <Modal
-          open={this.state.openDifficulty}
-          toggle={this.handleDifficultyClick}
-        >
-          <ModalHeader>Change Difficulty</ModalHeader>
-          <ModalBody>
-            <FormRadio
-              checked={this.state.difficulty === 'Beginner'}
-              onChange={() => {
-                this.changeDifficulty('Beginner');
-              }}
-            >
+          <Modal
+            open={this.state.openDifficulty}
+            toggle={this.handleDifficultyClick}
+          >
+            <ModalHeader>Change Difficulty</ModalHeader>
+            <ModalBody>
+              <FormRadio
+                checked={this.state.difficulty === 'Beginner'}
+                onChange={() => {
+                  this.changeDifficulty('Beginner');
+                }}
+              >
               Beginner
-            </FormRadio>
-            <FormRadio
-              checked={this.state.difficulty === 'Easy'}
-              onChange={() => {
-                this.changeDifficulty('Easy');
-              }}
-            >
+              </FormRadio>
+              <FormRadio
+                checked={this.state.difficulty === 'Easy'}
+                onChange={() => {
+                  this.changeDifficulty('Easy');
+                }}
+              >
               Easy
-            </FormRadio>
-            <FormRadio
-              checked={this.state.difficulty === 'Normal'}
-              onChange={() => {
-                this.changeDifficulty('Normal');
-              }}
-            >
+              </FormRadio>
+              <FormRadio
+                checked={this.state.difficulty === 'Normal'}
+                onChange={() => {
+                  this.changeDifficulty('Normal');
+                }}
+              >
               Normal
-            </FormRadio>
-            <FormRadio
-              checked={this.state.difficulty === 'Hard'}
-              onChange={() => {
-                this.changeDifficulty('Hard');
-              }}
-            >
+              </FormRadio>
+              <FormRadio
+                checked={this.state.difficulty === 'Hard'}
+                onChange={() => {
+                  this.changeDifficulty('Hard');
+                }}
+              >
               Hard
-            </FormRadio>
-            <Button onClick={this.handleDifficultyClick}>Accept</Button>
-          </ModalBody>
-        </Modal>
+              </FormRadio>
+              <Button onClick={this.handleDifficultyClick}>Accept</Button>
+            </ModalBody>
+          </Modal>
 
-        <Modal open={this.state.openRules} toggle={this.handleRulesClick}>
-          <ModalHeader>Welcome to Sudoku!</ModalHeader>
-          <ModalBody>
-            <div className='rulesText'>
-              <p>
+          <Modal open={this.state.openRules} toggle={this.handleRulesClick}>
+            <ModalHeader>Welcome to Sudoku!</ModalHeader>
+            <ModalBody>
+              <div className='rulesText'>
+                <p>
                 1. Only one number from 1-9 is allowed on each row<br></br>
-              </p>
-              <p>
+                </p>
+                <p>
                 2. Only one number from 1-9 is allowed on each column<br></br>
-              </p>
-              <p>
+                </p>
+                <p>
                 3. Only one number from 1-9 is allowed in each grid<br></br>
-              </p>
-              <p>
+                </p>
+                <p>
                 The goal of the game is to find the missing numbers in the grid
                 such that all three of these conditions are satisfied and if
                 they are then you have successfully completed the puzzle.
-                <br></br>
-              </p>
-              <p>
+                  <br></br>
+                </p>
+                <p>
                 If not, then you must backtrack and find out which numbers are
                 inserted incorrectly.<br></br>
-              </p>
-              <p>
+                </p>
+                <p>
                 You will know if the number is inserted incorrectly when the box
                 is highlighted red.<br></br>
-              </p>
-            </div>
-            <Button onClick={this.handleRulesClick}>Got it!</Button>
-          </ModalBody>
-        </Modal>
+                </p>
+              </div>
+              <Button onClick={this.handleRulesClick}>Got it!</Button>
+            </ModalBody>
+          </Modal>
 
-        <Modal open={this.state.openNewGame} toggle={this.handleNewGameClick}>
-          <ModalBody>
-            <div className='newGameText'>
+          <Modal open={this.state.openNewGame} toggle={this.handleNewGameClick}>
+            <ModalBody>
+              <div className='newGameText'>
               Are you sure?
-              <br />
-            </div>
-            <div className='flexButtons'>
-              <Button onClick={this.newGameAccepted}>Yes</Button>
-              <Button onClick={this.handleNewGameClick}>No</Button>
-            </div>
-          </ModalBody>
-        </Modal>
-      </div>
+                <br />
+              </div>
+              <div className='flexButtons'>
+                <Button onClick={this.newGameAccepted}>Yes</Button>
+                <Button onClick={this.handleNewGameClick}>No</Button>
+              </div>
+            </ModalBody>
+          </Modal>
+                  <div className='game'>
+          <p className='game-title title'>SUDOKU!</p>
+          <div className='game-board'>
+            <Board
+              difficulty={this.state.difficulty}
+              newGame={this.state.newGame}
+              populateGameGrid={this.populateGameGrid}
+              solvedButton={this.state.solvedButton}
+              solvedGrid={this.state.grid}
+            />
+          </div>
+        </div>
+      </>
     );
   }
 }
