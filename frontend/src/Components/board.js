96b257fa5d0581f9854798cb59e5dcfe9a2a8081
Jeremy Ng <jngchenghin@gmail.com>
2020-09-03 00:34:14 -0700

    converting Board to functional component

--- frontend/src/Components/board.js
+++ frontend/src/Components/board.js
@@ -1,41 +1,25 @@
 import React from 'react';
 import Row from './row';
 
-class Board extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      grid: [],
-      displayError: false,
-      beginTimer: 0,
-      timeUntilDismissed: 3,
-      complete: false,
-    };
-  }
-  
-  render() {
-    const { grid, complete, handleKeyPress } = this.props;
-
-    return (
-      <div className='sudoku'>
-        <div className='winCondition'>
-          {complete
-            ? 'You have successfully solved the sudoku!'
-            : 'You are not done yet!'}
-        </div>
-        {grid.map((row, rowNum) => (
-          <Row
-            key={rowNum}
-            grid={grid}
-            cells={row}
-            rowNum={rowNum}
-            handleKeyPress={handleKeyPress}
-          />
-        ))}
+const Board = ({ grid, complete, handleKeyPress }) => {
+  return (
+    <div className='sudoku'>
+      <div className='winCondition'>
+        {complete
+          ? 'You have successfully solved the sudoku!'
+          : 'You are not done yet!'}
       </div>
-    );
-  }
-}
+      {grid.map((row, rowNum) => (
+        <Row
+          key={rowNum}
+          grid={grid}
+          cells={row}
+          rowNum={rowNum}
+          handleKeyPress={handleKeyPress}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Board;
