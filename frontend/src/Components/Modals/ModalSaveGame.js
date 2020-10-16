d6e818a6f40bc4a60400041f5914c2e70c99160b
Jeremy Ng <jngchenghin@gmail.com>
2020-10-15 17:18:29 -0700

    Save game functionality with fetch calls!

--- frontend/src/Components/Modals/ModalSaveGame.js
+++ frontend/src/Components/Modals/ModalSaveGame.js
@@ -1,3 +1,4 @@
+import { useAuth0 } from '@auth0/auth0-react';
 import React, { useState, useEffect } from 'react';
 import {
   Modal,
@@ -5,27 +6,66 @@ import {
   ModalBody,
   FormInput,
   ModalFooter,
-  Button
+  Button,
 } from 'shards-react';
 import validateSaveName from '../../helperFn/validation';
 
 const ModalSaveGame = ({ open, setOpen }) => {
   const [saveName, setSaveName] = useState('');
-  const [saveGame, setSaveGame] = useState(false);
+  const [saveGameAccepted, setSaveGameAccepted] = useState(false);
+  const { getAccessTokenSilently, user } = useAuth0();
 
   //useEffect for handling save game success
   useEffect(() => {
     //fetch call along with validation of name
-    if (saveGame){
-      if (!validateSaveName(saveName)) {
-        alert('did not pass validation');
-      } else {
-        setOpen(false);
+    const savingGame = async () => {
+      if (saveGameAccepted) {
+        if (!validateSaveName(saveName)) {
+          alert('did not pass validation');
+        } else {
+          try {
+            const accessToken = await getAccessTokenSilently({
+              audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+              scope: process.env.REACT_APP_AUTH0_SCOPE,
+            });
+      
+            await fetch('http://localhost:3001/sudoku', {
+              method: 'POST',
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/json',
+              },
+              credentials: 'include',
+              body: JSON.stringify({
+                user_id: user.sub,
+                saveGame: {
+                  name: saveName,
+                  grid: sessionStorage.getItem('grid'),
+                  difficulty: sessionStorage.getItem('difficulty'),
+                  date: new Intl.DateTimeFormat('default', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                    second: 'numeric',
+                    hour12: true,
+                  }).format(new Date(Date.now())),
+                },
+              }),
+            });
+          } catch (e) {
+            console.log(e.message);
+          }
+
+          setOpen(false);
+        }
       }
-    }
-    setSaveGame(false);
-  }, [saveGame]);
-  
+      setSaveGameAccepted(false);
+    };
+    savingGame();
+  }, [saveGameAccepted]);
+
   return (
     <Modal open={open} toggle={setOpen}>
       <ModalHeader>Save Game</ModalHeader>
@@ -37,7 +77,7 @@ const ModalSaveGame = ({ open, setOpen }) => {
         />
       </ModalBody>
       <ModalFooter>
-        <Button onClick={() => setSaveGame(true)}>Save</Button>
+        <Button onClick={() => setSaveGameAccepted(true)}>Save</Button>
       </ModalFooter>
     </Modal>
   );
