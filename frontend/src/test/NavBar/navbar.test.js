19be03b2a7e7c5b6ae24885e9192a190170b59ac
Jeremy Ng <jngchenghin@gmail.com>
2020-10-24 01:12:23 -0700

    navbar authenticated and not authenticated test

--- frontend/src/test/NavBar/navbar.test.js
+++ frontend/src/test/NavBar/navbar.test.js
@@ -18,4 +18,15 @@ it('NavBar items, not authenticated', () => {
   expect(screen.getByText('Solve')).toBeTruthy();
   expect(screen.getByText(/How To Play/)).toBeTruthy();
   expect(screen.getByText(/New Game/)).toBeTruthy();
+  expect(screen.getByText(/Log In/)).toBeTruthy();
+});
+
+it('NavBar items, authenticated', () => {
+  render(<Router><NavBar navClickHandlers={mockClickHandlers} isAuthenticated /></Router>);
+  expect(screen.getByText('Credits')).toBeTruthy();
+  expect(screen.getByText('Difficulty')).toBeTruthy();
+  expect(screen.getByText('Solve')).toBeTruthy();
+  expect(screen.getByText(/How To Play/)).toBeTruthy();
+  expect(screen.getByText(/New Game/)).toBeTruthy();
+  expect(screen.getByText(/Log Out/)).toBeTruthy();
 });
