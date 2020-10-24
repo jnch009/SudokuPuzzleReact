e5281f68a736adfdbe2b8df048899793dd274b4a
Jeremy Ng <jngchenghin@gmail.com>
2020-10-24 01:10:04 -0700

    NavBar and NavBarItem tests

---
+++ frontend/src/test/NavBar/navbar.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import NavBar from '../../Components/NavBar/NavBar';
+
+const mockClickHandlers = {
+  handleCreditsClick: () => {},
+  handleDifficultyClick: () => {},
+  handleSudokuSolver: () => {},
+  handleRulesClick: () => {},
+  handleNewGameClick: () => {},
+};
+
+it('NavBar items, not authenticated', () => {
+  render(<Router><NavBar navClickHandlers={mockClickHandlers} /></Router>);
+  expect(screen.getByText('Credits')).toBeTruthy();
+  expect(screen.getByText('Difficulty')).toBeTruthy();
+  expect(screen.getByText('Solve')).toBeTruthy();
+  expect(screen.getByText(/How To Play/)).toBeTruthy();
+  expect(screen.getByText(/New Game/)).toBeTruthy();
+});
---
+++ frontend/src/test/NavBarItem/navbaritem.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NavBarItem from '../../Components/NavBarItem/NavBarItem';
+
+const name = 'Sudoku';
+
+it('NavBarItem component visible', () => {
+  render(<NavBarItem name={name} />);
+  expect(screen.getByRole('button')).toBeTruthy();
+  expect(screen.getByText(name)).toBeTruthy();
+});
