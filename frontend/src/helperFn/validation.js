3794ea9bd45dd4ee407d7b235279416a09445e44
Jeremy Ng <jngchenghin@gmail.com>
2020-10-26 21:42:51 -0700

    Validation fix (#32)

--- frontend/src/helperFn/validation.js
+++ frontend/src/helperFn/validation.js
@@ -1,7 +1,7 @@
 //validating the save game name on the frontend
 const regex = new RegExp(process.env.REACT_APP_REGEX_CURSE_WORDS);
 const validateSaveName = (name) => {
-  if (name.length === 0 || name.length > 100 || regex.test(name)) {
+  if (name.length === 0 || name.length > 100 || regex.test(name.toLowerCase())) {
     return false;
   }
 
