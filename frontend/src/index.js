ac744630e5aed293d62620e7653e462ec0a7ff8a
Jeremy Ng <jngchenghin@gmail.com>
2020-10-28 19:30:13 -0700

    service worker

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'shards-ui/dist/css/shards.min.css';
 import './index.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import Game from './Components/game';
 
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -21,3 +22,8 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
