d70cc2a14f90e81472e3fe505c10f211911c8f77
Jeremy Ng <jngchenghin@gmail.com>
2020-11-08 20:29:13 -0800

    removing multiline

--- frontend/src/Components/Square/squareUtility.js
+++ frontend/src/Components/Square/squareUtility.js
@@ -34,46 +34,21 @@ const setBorderColor = (row, col) => {
   return borderColorLookup(row, col) || '#c700ff';
 };
 
+// this is a complicated function, but I will explain as best as I can
+// what essentially is going on is we are populating the styled component button for each box
+// for example, rowTopLeft and rowTopLeft are corresponding to this order:
+// row 0 col 0, row 0 col 3, row 0 col 6, row 3 col 0, row 3 col 3, row 3 col 6, row 6 col 0, row 6 col 3, row 6 col 6
+// the boxes in those positions will all contain the top left border
+
 const boxConditionLookup = () => {
   const boxLookup = {};
 
-  boxLookupAssignment(
-    boxLookup,
-    rowTopLeft,
-    rowTopLeft,
-    SquareBorderTopLeft
-  );
-
-  boxLookupAssignment(
-    boxLookup,
-    rowTopLeft,
-    topRightRange,
-    SquareBorderTopRight
-  );
-
-  boxLookupAssignment(
-    boxLookup,
-    topRightRange,
-    rowTopLeft,
-    SquareBorderBottomLeft
-  );
-
-  boxLookupAssignment(
-    boxLookup,
-    topRightRange,
-    topRightRange,
-    SquareBorderBottomRight
-  );
-
+  boxLookupAssignment(boxLookup, rowTopLeft, rowTopLeft, SquareBorderTopLeft);
+  boxLookupAssignment(boxLookup, rowTopLeft, topRightRange, SquareBorderTopRight);
+  boxLookupAssignment(boxLookup, topRightRange, rowTopLeft, SquareBorderBottomLeft);
+  boxLookupAssignment(boxLookup, topRightRange, topRightRange, SquareBorderBottomRight);
   boxLookupAssignment(boxLookup, rowTopLeft, topBorder, SquareBorderTop);
-
-  boxLookupAssignment(
-    boxLookup,
-    topRightRange,
-    topBorder,
-    SquareBorderBottom
-  );
-
+  boxLookupAssignment(boxLookup, topRightRange, topBorder,SquareBorderBottom);
   boxLookupAssignment(boxLookup, topBorder, rowTopLeft, SquareBorderLeft);
   boxLookupAssignment(boxLookup, topBorder, topRightRange, SquareBorderRight);
 
