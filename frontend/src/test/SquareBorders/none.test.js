d62675c24c2a4f1cd49a4db99155d65af69c4297
Jeremy Ng <jngchenghin@gmail.com>
2020-11-08 18:27:12 -0800

    styled component snapshot testing

---
+++ frontend/src/test/SquareBorders/none.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import 'jest-styled-components';
+import NoBorder from '../../Components/Square/squareBorders/None';
+
+it('No border', () => {
+  const { container } = render(<NoBorder modify={true} />);
+  expect(container.firstChild).toMatchSnapshot();
+});
+
+it('No border not modifiable', () => {
+  const { container } = render(<NoBorder modify={false} />);
+  expect(container.firstChild).toMatchSnapshot();
+  expect(container.firstChild).toHaveStyleRule('color', '#fff');
+  expect(container.firstChild).toHaveStyleRule('cursor', 'not-allowed!important');
+});
---
+++ frontend/src/test/SquareBorders/right.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import 'jest-styled-components';
+import SquareBorderRight from '../../Components/Square/squareBorders/Right';
+
+const borderColor = 'blue';
+
+it('Right border', () => {
+  const { container } = render(<SquareBorderRight borderColor={borderColor} modify={true} />);
+  expect(container.firstChild).toMatchSnapshot();
+  expect(container.firstChild).toHaveStyleRule('border-right', `0.2em ${borderColor} solid`);
+});
+
+it('Right border not modifiable', () => {
+  const { container } = render(<SquareBorderRight borderColor={borderColor} modify={false} />);
+  expect(container.firstChild).toMatchSnapshot();
+  expect(container.firstChild).toHaveStyleRule('border-right', `0.2em ${borderColor} solid`);
+  expect(container.firstChild).toHaveStyleRule('color', '#fff');
+  expect(container.firstChild).toHaveStyleRule('cursor', 'not-allowed!important');
+});
