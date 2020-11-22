fef875f6201767ba11e97b921bd16ff91eed93cd
Jeremy Ng <jngchenghin@gmail.com>
2020-11-21 19:31:56 -0800

    Test names were incorrect

--- frontend/src/test/SquareState/danger.test.js
+++ frontend/src/test/SquareState/danger.test.js
@@ -4,7 +4,7 @@ import { squareStateConstants } from '../../helperFn/testHelpers';
 import SquareDangerBtn from '../../Components/Square/squareState/Danger';
 import NoBorder from '../../Components/Square/squareBorders/None';
 
-it('Square Active', () => {
+it('Square Danger', () => {
   const { container } = render(SquareDangerBtn(NoBorder, squareStateConstants.num, squareStateConstants.handleClick, squareStateConstants.borderColor));
   expect(container.firstChild).toMatchSnapshot();
   expect(container.firstChild.classList.contains('square')).toBeTruthy();
